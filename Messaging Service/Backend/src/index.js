4cf8185d0fc927d4164dff0074a8cf97edbad9c7
Shovin <shovindsouza27@gmail.com>
2024-12-30 22:01:57 +0530

    Few changes to the index

--- Messaging Service/Backend/src/index.js
+++ Messaging Service/Backend/src/index.js
@@ -1,9 +1,11 @@
 import express from "express";
-import authRoutes from "./routes/auth.routes.js";
-import messageRoutes from "./routes/message.routes.js";
 import dotenv from "dotenv" 
 import cookieParser from "cookie-parser"
+import cors from "cors";
+
 import {connectDB} from "./lib/db.js"
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 
 dotenv.config();
 const PORT = process.env.PORT
@@ -14,6 +16,10 @@ app.use(cookieParser());
 
 app.use("/api/auth",authRoutes);
 app.use("/api/message",messageRoutes);
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true 
+}))
 
 app.listen(PORT, ()=> {
     console.log("Server is running on PORT:"+PORT)
